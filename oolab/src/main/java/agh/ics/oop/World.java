909755fb45a980e99e0f23ed8ce1e54064cd9831
Ariel Michalik <arie.michalik@gmail.com>
2023-10-18 18:07:22 +0200

    do 13

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,20 @@
+package agh.ics.oop;
+
+public class World {
+    public static void run(String[] args){
+        System.out.println("zwierzak idzie do przodu");
+        for(int i = 0; i < args.length; i++){
+            if(i != args.length-1) {
+                System.out.print(args[i] + ", ");
+            }else{
+                System.out.print(args[i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("system wystartował");
+        run(args);
+        System.out.println("system zakończył działanie");
+    }
+
+}
