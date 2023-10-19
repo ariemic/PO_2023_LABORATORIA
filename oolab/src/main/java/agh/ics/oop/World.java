075a7bed4c4e322afcb3be40b2e92959dc8a3083
Ariel Michalik <arie.michalik@gmail.com>
2023-10-19 16:09:14 +0200

    zwierzak chodzi

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,15 +3,7 @@ package agh.ics.oop;
 public class World {
     public static void run(String[] args){
         System.out.println("Start");
-//        for(int i=0; i < args.length; i++){
-//            String message = args[i];
-//            switch(message){
-//                case "f" -> System.out.println("Zwierzak idzie do przodu");
-//                case "b" -> System.out.println("Zwierzak idzie do tyłu");
-//                case "r" -> System.out.println("Zwierzak skręca w prawo");
-//                case "l" -> System.out.println("Zwierzak skręca w lewo");
-//            }
-//        }
+
         for(String arg: args){
             String message = switch(arg){
                 case "f" -> "Zwierzak idzie do przodu";
