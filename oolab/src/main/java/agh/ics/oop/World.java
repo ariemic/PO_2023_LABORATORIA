c3e302656a9bc8ff942098dfb8aaabaf94f9348a
Ariel Michalik <arie.michalik@gmail.com>
2023-10-19 16:08:58 +0200

    zwierzak chodzi

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,15 +2,31 @@ package agh.ics.oop;
 
 public class World {
     public static void run(String[] args){
-        System.out.println("zwierzak idzie do przodu");
-        for(int i = 0; i < args.length; i++){
-            if(i != args.length-1) {
-                System.out.print(args[i] + ", ");
-            }else{
-                System.out.println(args[i]);
+        System.out.println("Start");
+//        for(int i=0; i < args.length; i++){
+//            String message = args[i];
+//            switch(message){
+//                case "f" -> System.out.println("Zwierzak idzie do przodu");
+//                case "b" -> System.out.println("Zwierzak idzie do tyłu");
+//                case "r" -> System.out.println("Zwierzak skręca w prawo");
+//                case "l" -> System.out.println("Zwierzak skręca w lewo");
+//            }
+//        }
+        for(String arg: args){
+            String message = switch(arg){
+                case "f" -> "Zwierzak idzie do przodu";
+                case "b" -> "Zwierzak idzie do tyłu";
+                case "r" -> "Zwierzak skręca w prawo";
+                case "l" -> "Zwierzak skręca w lewo";
+                default -> "";
+            };
+            if (!message.isEmpty()){
+                System.out.println(message);
             }
         }
+        System.out.println("Stop");
     }
+
     public static void main(String[] args) {
         System.out.println("system wystartował");
         run(args);
