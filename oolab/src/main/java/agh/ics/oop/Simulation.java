174fc90def368e009d9a21956cfc53150823e4b4
Ariel Michalik <arie.michalik@gmail.com>
2023-11-11 20:38:08 +0100

    Simulation done

--- oolab/src/main/java/agh/ics/oop/Simulation.java
+++ oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -1,4 +1,32 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.Animal;
+import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Simulation {
+    List<MoveDirection> moves;
+    List<Animal> animals = new ArrayList<Animal>();
+    public Simulation(List<Vector2d> animalPositions, List<MoveDirection> moves){
+        this.moves = moves;
+        //create new animals on animalPositions
+        for(Vector2d position: animalPositions){
+            animals.add(new Animal(position));
+        }
+    }
+
+    public void run(){
+        int i=0;
+        int animalCnt = animals.size();
+        for (MoveDirection move: moves){
+            animals.get(i%animalCnt).move(move);
+            System.out.printf("Zwięrzę %d : %s%n", i%animalCnt, animals.get(i%animalCnt).getVector().toString());
+            i++;
+        }
+
+    }
+
 }
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -5,6 +5,8 @@ import agh.ics.oop.model.MapDirection;
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
 
+import java.util.List;
+
 public class World {
 
     public static void run(MoveDirection[] args){
@@ -21,8 +23,8 @@ public class World {
 
     public static void main(String[] args) {
         System.out.println("Start");
-        MoveDirection[] directions = OptionsParser.parseDirections(args).toArray(new MoveDirection[0]);
-        run(directions);
+//        MoveDirection[] directions = OptionsParser.parseDirections(args).toArray(new MoveDirection[0]);
+//        run(directions);
         System.out.println("Stop");
         Vector2d position1 = new Vector2d(1,2);
         System.out.println(position1);
@@ -39,6 +41,11 @@ public class World {
         System.out.printf("animal position is %s%n", animal.getVector());
         animal.move(MoveDirection.BACKWARD);
         System.out.printf("animal position is %s%n",animal.getVector());
+
+        List<MoveDirection> directions = OptionsParser.parseDirections(args);
+        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+        Simulation simulation = new Simulation(positions, directions);
+        simulation.run();
 
     }
 
