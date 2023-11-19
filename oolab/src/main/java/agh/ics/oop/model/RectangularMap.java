112ee01d5220872731da0980b22dd4ae48e3eb75
Ariel Michalik <arie.michalik@gmail.com>
2023-11-19 15:02:42 +0100

    RectangularMap implements WorldMap

--- oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
+++ oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -1,4 +1,40 @@
 package agh.ics.oop.model;
 
-public class RectangularMap {
+import java.util.HashMap;
+import java.util.Map;
+
+public class RectangularMap implements WorldMap {
+    private Map<Vector2d, Animal> animals = new HashMap<>();
+    private final Integer width;
+    private final Integer height;
+
+    public RectangularMap(Integer width, Integer height){
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+        return false;
+    }
+
+    @Override
+    public boolean place(Animal animal) {
+        return false;
+    }
+
+    @Override
+    public void move(Animal animal, MoveDirection direction) {
+
+    }
+
+    @Override
+    public boolean isOccupied(Vector2d position) {
+        return false;
+    }
+
+    @Override
+    public Animal objectAt(Vector2d position) {
+        return null;
+    }
 }
