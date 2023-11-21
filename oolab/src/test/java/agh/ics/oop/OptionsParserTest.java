fd3d0fad1255edc67be7f774493c66b21fdd4c4c
Ariel Michalik <arie.michalik@gmail.com>
2023-11-21 14:41:50 +0100

    test OptionsParser for empty set

--- oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
+++ oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -10,5 +10,9 @@ public class OptionsParserTest {
         String[] args = {"f", "b", "r", "ah", "l", "nah", "meh"};
         MoveDirection[] expected = {MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.RIGHT, MoveDirection.LEFT};
         assertArrayEquals(expected, OptionsParser.parseDirections(args).toArray(new MoveDirection[0]));
+
+        String[] args2 = {};
+        MoveDirection[] expected2 = {};
+        assertArrayEquals(expected2, OptionsParser.parseDirections(args2).toArray(new MoveDirection[0]));
     }
 }
